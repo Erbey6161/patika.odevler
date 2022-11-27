fab44d4e8e7d99c5d1aaf5f763c9639a5f06e946
Name Surname <test@email.com>
2022-11-27 12:38:49 +0300

    Kullanıcı Girişi
    düzletme

--- KullaniciGirisi/src/Main.java
+++ KullaniciGirisi/src/Main.java
@@ -18,10 +18,24 @@ public class Main {
 
         if (userName.equals("şampiyon") && password.equals("127357")) {
             System.out.println("Tebrikler Giriş Yaptınız !!");
+
         } else {
             System.out.println("Hatalı Giriş Yaptınız !!");
+            System.out.println("Şifrenizi yenilemek istermisiniz ? (1:Evet 2:Hayır)");
+            int karar = scanner.nextInt();
+            if (karar == 1) {
+                System.out.println("yeni parola giriniz.");
+                password = scanner.nextLine();
+                if (password.equals("127357")) {
+                    System.out.println("Şifre oluşturulamadı başka bir şifre giriniz.");
+                } else if (password.equals("şampiyon")) {
+                    System.out.println("kullanıcı adınızdan farklı bir şifre giriniz.");
+                } else {
+                    System.out.println("şifre oluşturuldu");
+                }
+            }
+            System.out.println("iyi günler dileriz.");
         }
-
-
     }
+
 }
