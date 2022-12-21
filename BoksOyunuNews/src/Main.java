8f3d6046cedcf7304247db60e9d5e05b8acebfac
Name Surname <erenbodur64@gmail.com>
2022-12-21 08:46:21 +0300

    eklenti

--- BoksOyunuNews/src/Main.java
+++ BoksOyunuNews/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+   // Projeye ilk kimin dövüşe başlayacağını %50 olasılık ile hesaplayan sistemi entegre ediniz.
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Fighter f1 = new Fighter("Antony", 10, 120, 100,30);
+        Fighter f2 = new Fighter("Brian", 20, 85, 85,40);
+
+        Match match = new Match(f1, f2, 85, 100);
+        match.run();
     }
 }
