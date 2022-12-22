79423e5c3dd2360f4dcfeb1abec1000d6c2ce54e
Name Surname <erenbodur64@gmail.com>
2022-12-23 02:27:57 +0300

    Update Main.java

--- MaasHesaplayici/src/Main.java
+++ MaasHesaplayici/src/Main.java
@@ -8,7 +8,8 @@ public class Main {
     }
 }
 /*
- * ava'da "Employee" adında fabrika çalışanlarını temsil eden ve metotları ile çalışanların maaşlarını hesaplayan bir sınıf yazmalısınız. Bu sınıf 4 nitelik ve 5 metoda sahip olacaktır.
+ * java'da "Employee" adında fabrika çalışanlarını temsil eden ve metotları ile çalışanların maaşlarını hesaplayan
+bir sınıf yazmalısınız. Bu sınıf 4 nitelik ve 5 metoda sahip olacaktır.
 Sınıfın Nitelikleri
 name : Çalışanın adı ve soyadı
 salary : Çalışanın maaşı
